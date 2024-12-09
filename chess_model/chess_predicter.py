b6d3eb031e496d366776f68e6c2b2cc83d2194a3
unknown <lin13445@gmail.com>
2024-12-09 21:35:48 +0700

    add class

--- chess_model/chess_predicter.py
+++ chess_model/chess_predicter.py
@@ -1,44 +1,15 @@
-import os
-import numpy as np
-from PIL import Image
-from ultralytics import YOLO
-from dotenv import dotenv_values
-from ultralytics.yolo.utils.ops import scale_image
-
-config = dotenv_values("./.env") 
+from ultralytics.utils.ops import xyxy2xywh
 
 class ChessPredicter:
     def __init__(self, model):
-      self.model_path = model
+      self.model = model
 
-    def predict_image(self, img):
-      result = model(img)[0]
-
-      # detection
-      # result.boxes.xyxy   # box with xyxy format, (N, 4)
-      cls = result.boxes.cls.cpu().numpy()    # cls, (N, 1)
-      probs = result.boxes.conf.cpu().numpy()  # confidence score, (N, 1)
-      boxes = result.boxes.xyxy.cpu().numpy()   # box with xyxy format, (N, 4)
-
-      # segmentation
-      masks = result.masks.masks.cpu().numpy()     # masks, (N, H, W)
-      masks = np.moveaxis(masks, 0, -1) # masks, (H, W, N)
-      # rescale masks to original image
-      masks = scale_image(masks.shape[:2], masks, result.masks.orig_shape)
-      masks = np.moveaxis(masks, -1, 0) # masks, (N, H, W)
-
-      return boxes, masks, cls, probs
+    def predict_image(self, img_path):
+      results = self.model(img_path)[0]
+      return results.to_json()
 
     def predict_video(self):
       pass
     
     def get_base_coordinates(self, xywh):
-      print(xywh[0])
-      return xywh[0]
-       
-
-print(config.values())
-
-model = YOLO(config["PROJECT_PATH"] + "/chess_model/runs/detect/chess_data_model/weights/best.pt") # path to .pt model file
-
-results = model("./test_data/2_move_student.mp4", stream=True, save=True, show=True, project='./result',)
+      pass
